c5ff080706d89f36991f08dfee1af662d6ab5c92
dhart043 <dalton.hart@ascension.org>
2025-05-23 09:12:23 -0700

    [feat] Enhance FeedPage with avatar dropdown functionality and update Next.js configuration. Added image handling for Discord avatars, implemented dropdown menu for user settings and sign-out, and configured remote image patterns in next.config.js for improved image loading.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-/** @type {import('next').NextConfig} */ const nextConfig = {}; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
