adf9ed23828e7d94da91a8aee688c759eefddbf2
dhart043 <dalton.hart@ascension.org>
2025-05-23 10:49:32 -0700

    [chore] Update Jest configuration and enhance test coverage. Modified jest.backend.config.js and jest.config.js to include coverage collection for relevant directories, updated test scripts in package.json to run with coverage, and adjusted jest.setup.ts for improved testing setup. Updated package-lock.json and package.json to reflect dependency changes, including Next.js version bump.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,24 @@
-module.exports = {
-  preset: 'ts-jest',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/e2e/', '/app/api/.*\\.test\\.[jt]sx?$'],
   testMatch: ['<rootDir>/app/**/*.test.[jt]s?(x)'],
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx,ts,tsx}',
+    'lib/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
 };
+
+module.exports = createJestConfig(customJestConfig);
